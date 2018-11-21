a2ff68a195b8af4cfab14fd8e90bb7b3f570efd0
Tarik-INC <tarikesmin@gmail.com>
2018-11-21 17:26:19 -0200

    Added 'proof of work' image and  added filename as command arg

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from turing_machine import MT
-
+import sys
 if __name__ == "__main__":
 
+    filename = sys.argv[-1]
     try:
-        machine = MT.build_from_file('./test.txt')
+        machine = MT.build_from_file(filename)
         machine.start_computation()
     except Exception as ex:
         print(ex)
